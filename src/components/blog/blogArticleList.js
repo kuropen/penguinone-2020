04927e8f314feec6b228a20b10f1038d0869d7ec
Hirochika Yuda <hyuda@kuropen.me>
2020-08-09 16:56:44 +0900

    Replace additional occurences

--- src/components/blog/blogArticleList.js
+++ src/components/blog/blogArticleList.js
@@ -1,28 +1,30 @@
 import React from "react";
-import {Link, graphql, useStaticQuery} from "gatsby";
+import {Link, graphql, StaticQuery} from "gatsby";
 import { RichText } from 'prismic-reactjs';
 
-export default ({className, max = 0}) => {
-    const {prismic} = useStaticQuery(query);
-    const articles = prismic.allBlogs.edges;
-    let shownCount = 0;
-    const listElements = articles.map((article) => {
-        const {title, posting_date, _meta} = article.node;
-        const id = _meta.uid;
-        shownCount++;
-        if (max > 0 && shownCount > max) {
-            return null;
-        }
-        return (
+export default ({className, max = 0}) => (
+  <StaticQuery query={query} render={data => {
+      const {prismic} = data;
+      const articles = prismic.allBlogs.edges;
+      let shownCount = 0;
+      const listElements = articles.map((article) => {
+          const {title, posting_date, _meta} = article.node;
+          const id = _meta.uid;
+          shownCount++;
+          if (max > 0 && shownCount > max) {
+              return null;
+          }
+          return (
             <li key={id}><Link to={`/blog/${id}`}>{RichText.asText(title)} ({posting_date})</Link></li>
-        );
-    });
-    return (
+          );
+      });
+      return (
         <ul className={className}>
             {listElements}
         </ul>
-    );
-};
+      );
+  }} />
+);
 
 const query = graphql`
   query BlogArticleListQuery {
