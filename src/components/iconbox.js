732362297d339ae5d3dc39f8859388daf135002f
Hirochika Yuda <hyuda@kuropen.me>
2020-08-10 10:29:34 +0900

    Layout fix on smartphone

--- src/components/iconbox.js
+++ src/components/iconbox.js
@@ -12,9 +12,9 @@ export default ({children, className, icon}) => {
   return (
     <Box className={classNames(defaultClasses, extraClasses)}>
       <div className="mr-4 flex-shrink-0">
-        <span className="text-5xl">{icon}</span>
+        <span className="text-4xl md:text-5xl">{icon}</span>
       </div>
-      <div className="flex-grow">
+      <div className="flex-grow break-all sm:break-normal">
         {children}
       </div>
     </Box>
