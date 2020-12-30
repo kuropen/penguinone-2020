4b2dd5166f687393a952f0309d50c70c7abaa0d9
Hirochika Yuda <hyuda@kuropen.me>
2020-12-30 16:34:22 +0900

    Restore route to tech articles

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,13 +2,17 @@ import React from "react";
 import Layout from "../components/layout";
 import BlogArticleList from "../components/blog/blogArticleList";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBook } from "@fortawesome/free-solid-svg-icons";
+import { faBook, faJournalWhills } from "@fortawesome/free-solid-svg-icons";
 import Iconbox from "../components/iconbox";
 import CreativeCommons from "../components/cc/creativeCommons";
+import NavLinkBox from "../components/navLinkBox";
 
 export default () => {
     return (
         <Layout pageTitle="Blog">
+          <div className="m-1 md:m-2">
+            <NavLinkBox href="/tech" type="Link" icon={faJournalWhills} text="2018年以前の技術関連記事はこちら" />
+          </div>
           <Iconbox className="kpBlogIndexBorder" icon={<FontAwesomeIcon icon={faBook}/>}>
             <h2 className="text-xl text-bold font-orbitron">Blog Articles</h2>
             <BlogArticleList />
